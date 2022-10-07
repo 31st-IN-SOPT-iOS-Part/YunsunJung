205a8b6aa15ffb245f5c945d49fbebd31ff39300
Yunsun Jung <yunsun992510@gmail.com>
2022-10-07 20:51:36 +0900

    br/#1: feat/View UI 최종 완성

---
+++ FirstAssignment/FirstAssignment/HasLoggedInViewController.swift
@@ -0,0 +1,48 @@
+//
+//  HasLoggedInViewController.swift
+//  FirstAssignment
+//
+//  Created by 정윤선 on 2022/10/06.
+//
+
+import UIKit
+
+class HasLoggedInViewController: UIViewController {
+
+    private let welcomeLabel: UILabel = {
+        let label = UILabel(frame: CGRect(x: 20, y: 285, width: 350, height: 100))
+        label.numberOfLines = 2
+        label.text = "000님\n환영합니다"
+        label.font = .boldSystemFont(ofSize: 23)
+        
+        // 줄간격 늘리는 코드.. 원리는 아직 잘 모르겠어요ㅜㅜ
+        let attrString = NSMutableAttributedString(string: label.text!)
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = 15
+        attrString.addAttribute(NSAttributedString.Key.paragraphStyle, value: paragraphStyle, range: NSMakeRange(0, attrString.length))
+        label.attributedText = attrString
+        label.textAlignment = .center
+        
+        return label
+    }()
+    
+    private let signUpButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 20, y: 515, width: 350, height: 54))
+        button.setTitle("확인", for: .normal)
+        button.setTitleColor(UIColor(red: 0.1, green: 0.1, blue: 0.1, alpha: 1.0), for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 23)
+        button.backgroundColor = UIColor(red: 0.98, green: 0.89, blue: 0.3, alpha: 1.0)
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        let components: [Any] = [welcomeLabel, signUpButton]
+        components.forEach {
+            view.addSubview($0 as! UIView)
+        }
+    }
+    
+}
--- FirstAssignment/FirstAssignment/SceneDelegate.swift
+++ FirstAssignment/FirstAssignment/SceneDelegate.swift
@@ -22,7 +22,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                 let window = UIWindow(windowScene: windowScene)
                 window.overrideUserInterfaceStyle = UIUserInterfaceStyle.light
                 
-                let rootVC = SignUpViewController()
+                let rootVC = HasLoggedInViewController()
                 let navigationController = UINavigationController(rootViewController: rootVC)
                 
                 window.rootViewController = navigationController
